3d5cd1bc13ac3ddf86b56b3e8f22661ced4b9ca3
29017 <Michael.Killeen@icfi.com>
2015-05-15 14:06:19 -0400

    Update .gitignore

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 29017 on 5/15/2015.
+ */
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337');
+
+
